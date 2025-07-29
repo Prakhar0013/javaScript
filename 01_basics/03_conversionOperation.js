f0dd98579514d9f042128e373371e8c60b5f8656
Prakhar0013 <72659712+Prakhar0013@users.noreply.github.com>
2025-07-29 12:20:21 +0000

    Conversion Operations in JS

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score ="A"
+
+//console.log(typeof(score));
+
+let valueInNumber= Number(score);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);  
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1; false=>0;
+
+let isLoggedIn= 0;
+let booleanIsLoggedIn= Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//""=>false
+//"Prakhar"=>true
+//0=>false; any integer other than 0=>true
+let someNumber=undefined
+let stringNumber= String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
